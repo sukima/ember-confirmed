52c1d9dab33c87d4a8c4acedc19d7515ee514130
Devin Weaver <suki@tritarget.org>
2017-09-08 15:36:52 -0400

    Import vendor trees during build

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 /* eslint-env node */
 'use strict';
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
-  name: 'ember-confirmed'
+  name: 'ember-confirmed',
+
+  included() {
+    this._super.included.apply(this, arguments);
+    this.import('vendor/confirmer.js');
+    this.import('vendor/shims/confirmer.js');
+  },
+
+  treeForVendor(vendorTree) {
+    var confirmedPath = path.join(this.project.root, 'node_modules', 'confirmed', 'dist');
+    var confirmedTree = new Funnel(confirmedPath, {
+      files: ['confirmer.js']
+    });
+
+    return new MergeTrees([vendorTree, confirmedTree]);
+  }
 };
